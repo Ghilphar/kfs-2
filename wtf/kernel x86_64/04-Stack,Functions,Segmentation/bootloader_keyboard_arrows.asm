b45d3610298f954cb318b71ab4280c8cc366618b
Ghilphar <felipegaribotti@gmail.com>
2023-07-06 15:30:15 +0200

    last

--- wtf/kernel x86_64/04-Stack,Functions,Segmentation/bootloader_keyboard_arrows.asm
+++ wtf/kernel x86_64/04-Stack,Functions,Segmentation/bootloader_keyboard_arrows.asm
@@ -5,24 +5,13 @@ int 0x10            ; Clear screen
 
 mov ax, 0xB800
 mov es, ax          ; ES points to video memory
-mov di, 0            ; DI points to the cursor
+mov di, 0           ; DI points to the cursor
 mov cl, 0x07        ; Initial color
 
 get_input:
-    xor ah, ah
-    int 0x16  ; Get the input from the keyboard
+    mov ah, 0x00
+    int 0x16  ; Wait for a keypress
 
-    cmp al, 0xE0            ; When you press a special key like an arrow key, the BIOS generates a two-byte scancode. The first byte for these keys is always 0xE0
-    jne get_input
-
-    xor ah, ah
-    int 0x16  ; Get the second byte of the scan code
-
-    cmp ah, 0x4B  ; Left arrow key
-    jne get_input
-
-    sub di, 2  ; Move the cursor two positions to the left
-    mov al, 'C'
     mov [es:di], al
     mov [es:di+1], cl
     jmp get_input  ; Jump back to the input loop

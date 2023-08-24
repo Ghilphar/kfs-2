5d0be7d7e6ab2850e87946400629f2052f81c945
RadioPotin <laradiopotin@gmail.com>
2023-08-24 16:47:26 +0200

    changes to comments, some nasm conventions and other

--- kernel.s
+++ kernel.s
@@ -1,6 +1,7 @@
 section .data
 vga_buffer_ptr: dd 0xb8000   ; VGA text buffer start address
 bytes_to_print: db 64        ; Number of bytes to print (change as needed)
+attribute_byte: db 0x0F      ; White text on black background
 
 section .text
 global printk
@@ -19,48 +20,55 @@ printk:
 print_loop:
     cmp ecx, 0
     je done
-
+    
     ; Load byte from memory address edi
     xor eax, eax
     mov al, [edi]
-
+    
     ; Convert byte to two hex characters
     call byte_to_hex
 
-    ; Print first hex character
+    ; Get the current VGA buffer address
     mov edx, [vga_buffer_ptr]
+
+    ; Print first hex character
     mov byte [edx], al
+    mov al, [attribute_byte]
+    mov byte [edx + 1], al
     add edx, 2
-    mov [vga_buffer_ptr], edx
 
     ; Print second hex character
-    mov edx, [vga_buffer_ptr]
     mov byte [edx], ah
+    mov al, [attribute_byte]
+    mov byte [edx + 1], al
     add edx, 2
-    mov [vga_buffer_ptr], edx
 
     ; Print a space for separation
-    mov edx, [vga_buffer_ptr]
     mov al, ' '
     mov byte [edx], al
+    mov al, [attribute_byte]
+    mov byte [edx + 1], al
     add edx, 2
+
+    ; Update the VGA buffer pointer
     mov [vga_buffer_ptr], edx
 
-    ; Advance to the next byte and loop
-    inc  edi
-    dec  ecx
-    test ecx, 15   ; Check if we've printed 16 bytes
-    jnz  print_loop
+    ; Advance to the next byte and check if you've printed 16 bytes
+    inc edi
+    dec ecx
+    test ecx, 15
+    jnz print_loop
 
     ; Add a newline after every 16 bytes
     call newline
-    jmp  print_loop
+    jmp print_loop
+
 
 newline:
     ; Print a newline to the VGA
     push ebx
-    mov  ebx, 64
-    add  [vga_buffer_ptr], ebx
+    mov ebx, 64  ; Move to the next line
+    add [vga_buffer_ptr], ebx
     pop  ebx
     ret
 
@@ -80,7 +88,6 @@ byte_to_hex:
     and  ebx, 0x0F     ; Mask high nibble
     call nibble_to_hex; Convert high nibble to hex character in al
     mov  ah, al        ; Store high nibble in ah
-
     and  eax, 0x0F     ; Mask to get the low nibble
     call nibble_to_hex; Convert low nibble to hex character in al
 
@@ -100,7 +107,7 @@ not_alpha:
 
 kernel_main:
     pusha
-    mov edi, vga_buffer_ptr
+    mov edi, esp
     call printk
     popa
     ret
